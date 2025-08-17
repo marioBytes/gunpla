b146c806d1ddc902e142504c5daec5a58f688a01
Mario <andresmart92@gmail.com>
2025-08-17 17:06:08 -0600

    Creates JSON fallbacks

---
+++ backend/lib/regalia_web/controllers/fallback_controller.ex
@@ -0,0 +1,30 @@
+defmodule RegaliaWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use RegaliaWeb, :controller
+
+  # This clause handles errors returned by Ecto's insert/update/delete.
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(json: RegaliaWeb.ChangesetJSON)
+    |> render(:error, changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(html: RegaliaWeb.ErrorHTML, json: RegaliaWeb.ErrorJSON)
+    |> render(:"404")
+  end
+
+  def call(conn, {:error, :unauthorized}) do
+    conn
+    |> put_status(:unauthorized)
+    |> put_view(json: RegaliaWeb.ErrorJSON)
+    |> render(:"401")
+  end
+end

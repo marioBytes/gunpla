78c4155e8cc4cc6988afaa6c29497300837be63d
Mario <andresmart92@gmail.com>
2025-09-15 12:58:58 -0600

    Infinite loop when logging in

--- client/src/routes/_authenticated/_confirm.tsx
+++ client/src/routes/_authenticated/_confirm.tsx
@@ -3,7 +3,7 @@ import { Outlet, createFileRoute, redirect } from '@tanstack/react-router'
 export const Route = createFileRoute('/_authenticated/_confirm')({
   component: () => <Outlet />,
   beforeLoad: ({ context }) => {
-    if (!context.auth.user?.confirmed_at) {
+    if (context.auth.user!.confirmed_at === null) {
       throw redirect({
         to: '/confirm-email',
       })
